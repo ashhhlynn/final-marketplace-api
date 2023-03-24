207c96c4dec5eb87a0cf5984382733269049464a
ashhhlynn <ashleylynnwelch@gmail.com>
2023-03-24 17:15:23 -0500

    test

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :products
   resources :users
 
-  get 'render/index'
-  root 'render#index'
+  root "products#index"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
