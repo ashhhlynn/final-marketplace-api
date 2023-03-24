c1937f83bf5f7eb164b3f962a0a27af68cddfb2e
ashhhlynn <ashleylynnwelch@gmail.com>
2023-03-24 10:47:36 -0500

    test

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,8 @@ module FinalMarketplaceApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   resources :order_items
   resources :products
   resources :users
+
+  get 'render/index'
+  root 'render#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
