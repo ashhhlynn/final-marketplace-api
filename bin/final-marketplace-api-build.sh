b6c8097c3e67d31a41764b9cf8fb23a0d53a3f8e
ashhhlynn <ashleylynnwelch@gmail.com>
2023-05-30 07:41:00 -0500

    test

--- bin/final-marketplace-api-build.sh
+++ bin/final-marketplace-api-build.sh
@@ -3,8 +3,8 @@ set -o errexit
 
 bundle install
 # bundle exec rails assets:clean
-bundle exec rake db:migrate
-bundle exec rake db:seed
+bundle exec rails db:migrate
+bundle exec rails db:seed
 
 
 #if you have seeds to run add:
