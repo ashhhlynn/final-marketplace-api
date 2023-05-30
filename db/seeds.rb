9330a3b0a286d3183b466ec85c6464e9ac7f8595
ashhhlynn <ashleylynnwelch@gmail.com>
2023-05-30 08:22:48 -0500

    test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Product.create!(title: "Star Jasmine", price:"17", user_id: 1, image_url:"https://i.etsystatic.com/26683703/r/il/d0b6b3/4914577738/il_1588xN.4914577738_mjll.jpg", description:"Water a lot in the summer, test", sold:"false")
 
-User.create!(name:"Ashley", email:"ashley@gmail.com", address:"144 st", password:"tnns12345", password_confirmation:"tnns12345")
+User.create!(name:"Ashley", email:"ashley@gmail.com", address:"144 st", password:"tnns12345", password_confirmation:"tnns12345", city:"Chicago", state:"Illinois", zip:"66610")
+Product.create!(title: "Star Jasmine", price:"17", user_id: 1, image_url:"https://i.etsystatic.com/26683703/r/il/d0b6b3/4914577738/il_1588xN.4914577738_mjll.jpg", description:"Water a lot in the summer, test", sold:"false")
