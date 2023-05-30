f62d1cc4db072513b592c837b7662cba17b591f6
ashhhlynn <ashleylynnwelch@gmail.com>
2023-05-30 09:45:12 -0500

    test

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,3 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create!(name:"Ashley", email:"ashley@gmail.com", address:"144 st", password:"tnns12345", password_confirmation:"tnns12345", city:"Chicago", state:"Illinois", zip:"66610")
-Product.create!(title: "Star Jasmine", price:"17", user_id: 1, image_url:"https://i.etsystatic.com/26683703/r/il/d0b6b3/4914577738/il_1588xN.4914577738_mjll.jpg", description:"Water a lot in the summer, test", sold:"false")
