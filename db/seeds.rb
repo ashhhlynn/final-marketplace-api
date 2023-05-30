4d8c8d657593147d1e9be274664209449115ecd5
ashhhlynn <ashleylynnwelch@gmail.com>
2023-05-30 07:32:51 -0500

    test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.create(title: "Star Jasmine", price:"17", image_url:"https://i.etsystatic.com/26683703/r/il/d0b6b3/4914577738/il_1588xN.4914577738_mjll.jpg", description:"Water a lot in the summer, test", sold:"false")
