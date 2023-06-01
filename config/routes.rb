738ccbbc21c45dcb2d97b00aa78b28f33b3845cc
ashhhlynn <ashleylynnwelch@gmail.com>
2023-06-01 07:54:54 -0500

    test charges route

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   post '/login', to: 'auth#create'
   get '/profile', to: 'users#profile'
+  post '/charges', to: 'charges#create'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
