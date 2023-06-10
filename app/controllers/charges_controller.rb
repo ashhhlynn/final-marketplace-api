59318b49e913d3825304e14793ddb7ac21fdeb28
ashhhlynn <ashleylynnwelch@gmail.com>
2023-06-10 18:44:18 -0500

    test stripe

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -3,7 +3,9 @@ require 'dotenv'
 Dotenv.load
 
 class ChargesController < ApplicationController
+    
     skip_before_action :authorized
+
     def create
        amount = params[:price]
        token = params[:charge][:token]
