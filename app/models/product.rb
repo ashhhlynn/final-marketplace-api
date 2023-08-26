136a2ca112a16e23de61cfe15f4afe13159ba587
ashhhlynn <ashleylynnwelch@gmail.com>
2023-08-25 21:44:26 -0500

    test validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,7 @@ class Product < ApplicationRecord
     has_many :orders, through: :order_items
     belongs_to :user
 
+    validates :description, length: { maximum: 300 }
+    validates :title, length: { maximum: 50 }
+    validates :price, length: { maximum: 12 }
 end 
--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,6 +1,5 @@
 class OrderSerializer < ActiveModel::Serializer
     attributes :user_id, :id, :total, :updated_at, :complete
-    has_many :order_items
-    
+    has_many :order_items   
     belongs_to :user
 end
